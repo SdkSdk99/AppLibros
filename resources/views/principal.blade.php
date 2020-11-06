cc934c0528db48b11ad99a1ef2792ca629ae2ea1
Roymrodriguezxd <royman12351@gmail.com>
2020-11-05 21:39:52 -0500

    arreglo vista principal

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -76,9 +76,9 @@
         </div>
     </div>
     <footer class="app-footer">
-        <span><a href="http://www.ADSI.com/">Roy M</a> &copy; 2020</span>
+        <span><a href="http://www.ADSI.com/">Roy M Rodriguez</a> &copy; 2020</span>
         <!-- /Fin del contenido principal -->
-        <span class="ml-auto">Desarrollado por <a href="http://www.ADSI.com/">ADSI</a></span>
+        <span class="ml-auto">Desarrollado por <a href="https://www.instagram.com/el.royy/?hl=es">Roy</a></span>
     </footer>
     <script src="js/app.js"></script>
     <script src="js/plantilla.js"></script>
